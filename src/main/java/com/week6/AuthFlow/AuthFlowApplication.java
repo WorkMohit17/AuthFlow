5b37757e7dd061327c97fc8be5e93634b549bba1
Mohit Bansal <workmohit17@gmail.com>
2026-07-04 17:53:14 +0530

    initial commit

---
+++ src/main/java/com/week6/AuthFlow/AuthFlowApplication.java
@@ -0,0 +1,13 @@
+package com.week6.AuthFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/week6/AuthFlow/AuthFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week6.AuthFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
