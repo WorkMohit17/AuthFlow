80614e4151c945abeb86619b3c9db712f631ff0c
Mohit Bansal <workmohit17@gmail.com>
2026-07-04 18:40:49 +0530

    added userService to loadUserByEmail and getUserById. Added userRepository with method to findUserByEmail

---
+++ src/main/java/com/week6/AuthFlow/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.week6.AuthFlow.repositories;
+
+import com.week6.AuthFlow.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
---
+++ src/main/java/com/week6/AuthFlow/services/UserService.java
@@ -0,0 +1,31 @@
+package com.week6.AuthFlow.services;
+
+import com.week6.AuthFlow.entities.UserEntity;
+import com.week6.AuthFlow.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(email)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public UserEntity getUserById(Long userId){
+        return userRepository
+                .findById(userId)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+}
